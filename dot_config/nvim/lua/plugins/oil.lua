4cce02f02cf05e105db2d528b012aaed7ee0ff9d
Alex Servin II <servin.alejandroii@gmail.com>
2026-04-21 22:24:30 -0500

    oil config update

--- dot_config/nvim/lua/plugins/oil.lua
+++ dot_config/nvim/lua/plugins/oil.lua
@@ -1,11 +1,16 @@
 return {
   "stevearc/oil.nvim",
-  ---@module 'oil'
-  ---@type oil.SetupOpts
-  opts = {},
-  -- Optional dependencies
-  dependencies = { { "nvim-mini/mini.icons", opts = {} } },
-  -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
-  -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
-  lazy = false,
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    require("oil").setup({
+      -- Set to true to use oil as the default file explorer
+      default_file_explorer = true,
+      -- Optional: Set keymap to open oil
+      view_options = {
+        show_hidden = true,
+      },
+    })
+    vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+    vim.keymap.set("n", "<leader>e", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+  end,
 }
